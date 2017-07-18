e793728298b87c4ca1bef1ae210319010a611e00
slammyde7113 <dyers1@wit.edu>
2017-07-18 10:52:17 -0400

    User Model Modification
    changed this:
    class User < ApplicationRecord
      include Authentication
      has_many :examples, :articles, :comments
    end
    
    to this:
    class User < ApplicationRecord
      include Authentication
      has_many :examples
      has_many :articles
      has_many :comments
    end
    
    Previous model treated articles and comments as options
    instead of references.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 class User < ApplicationRecord
   include Authentication
-  has_many :examples, :articles, :comments
+  has_many :examples
+  has_many :articles
+  has_many :comments
 end
