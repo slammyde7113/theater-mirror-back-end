6f9fe41469dc9851df9489fd2cb2e0c270078c7a
slammyde7113 <dyers1@wit.edu>
2017-07-19 13:29:01 -0400

    Typo from last commit: Re-added guest column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170719171541) do
+ActiveRecord::Schema.define(version: 20170719172510) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20170719171541) do
     t.datetime "updated_at",                      null: false
     t.boolean  "admin",           default: false
     t.boolean  "user",            default: false
+    t.boolean  "guest"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["token"], name: "index_users_on_token", unique: true, using: :btree
   end
